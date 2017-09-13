b8dcfb860d7ac42e54fb999f38ec78f3138bcf4c
camilo8708 <cc.huertas@uniandes.edu.co>
2017-09-13 00:21:04 -0500

    Dolibarr - Cypress

--- Dolibarr/cypress/integration/Comercial.js
+++ Dolibarr/cypress/integration/Comercial.js
@@ -1,5 +1,5 @@
-describe('Dolibarr Login', function() {
-    it('autenticación Dolibarr', function() {
+describe('Dolibarr Modulo Comercial', function() {
+    it('Dolibarr - Crear Presupuesto', function() {
         cy.visit('http://52.54.130.109/dolibarr')
         cy.get('.login_table').find('input[id="username"]').click().type("admin")
         cy.get('.login_table').find('input[name="password"]').click().type("miso2017")
@@ -21,7 +21,7 @@ describe('Dolibarr Login', function() {
         cy.get('.border').find('select[name="availability_id"]').select('3')
         cy.get('.border').find('select[name="shipping_method_id"]').select('1')
         cy.get('.border').find('button[id="date_livraisonButtonNow"]').click()
-        //cy.get('.border').find('input[name="date_livraison"]').contains('04/09/2017')
+        
         cy.get('.border').find('input[name="date_livraison"]').clear().type('04/09/2007')
         cy.get('.border').find('textarea[name="note_public"]').type('Prueba nota publica')
         cy.get('.border').find('textarea[name="note_private"]').type('Prueba nota privada')
@@ -41,11 +41,48 @@ describe('Dolibarr Login', function() {
         cy.get('.border').find('input[name="re"]').clear().type('25/08/1987')
 
         cy.get('.center').contains('Crear borrador').click()
+    })
 
-
+    it('Dolibarr - Crear Pedido', function() {
+        cy.visit('http://52.54.130.109/dolibarr')
+        cy.get('.login_table').find('input[id="username"]').click().type("admin")
+        cy.get('.login_table').find('input[name="password"]').click().type("miso2017")
+        cy.get('.login_table').contains('Connection').click()
         
-        
+        cy.get('.tmenucenter').contains('Comercial').click()
 
+        cy.get('.vmenu').find('a.vsmenu').contains('Nuevo pedido').click()
+
+        cy.get('.border').find('input[name="ref_client"]').type(123)
+        
+   
+        
+        cy.get('.border').find('input[name="re"]').clear()
+
+        cy.get('.center').contains('Crear borrador').click()
+        //Validación de mensajes de error
+        cy.get('.jnotify-message').contains('El campo \'Cliente\' es obligatorio')
+        cy.get('.jnotify-message').contains('El campo \'Fecha\' es obligatorio')
+
+        cy.get('.border').find('input[id="s2id_autogen1"]').click({force: true}).type("s")
+        cy.get('.select2-results').contains('sdgsdg dsfsdf (sdgsdg)').click()
+
+        cy.get('.border').find('input[name="re"]').clear()
+        cy.get('.border').find('button[id="reButtonNow"]').click()
+        cy.get('.border').find('button[id="liv_ButtonNow"]').click()
+        cy.get('.border').find('select[name="cond_reglement_id"]').select('5')
+        cy.get('.border').find('select[name="mode_reglement_id"]').select('4')
+        cy.get('.border').find('select[name="availability_id"]').select('2')
+        cy.get('.border').find('select[name="shipping_method_id"]').select('2')
+        cy.get('.border').find('select[name="demand_reason_id"]').select('3')
+        
+        cy.get('.border').find('textarea[name="note_public"]').type('Prueba nota publica')
+        cy.get('.border').find('textarea[name="note_private"]').type('Prueba nota privada')
+        
+        cy.get('.center').contains('Crear borrador').click()
+        
+        cy.get('.fiche').contains('123')
+        
     })
 })
 
